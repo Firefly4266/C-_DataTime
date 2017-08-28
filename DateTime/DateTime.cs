34219e016e2cfd38be497d686ca9fddc27056a24
Firefly4266 <gaines.johnp@gmail.com>
2017-08-27 20:48:40 -0700

    completed exercise

---
+++ DateTime/DateTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Date_Time
+{
+    public static void Main(string[] args)
+    {
+        var dateTime = new DateTime(1965, 04, 18);
+        var now = DateTime.Now;
+        var today = DateTime.Today;
+        Console.WriteLine("Hour:" + now.Hour);
+        Console.WriteLine("minute: " + now.Minute);
+
+        var tomorrow = now.AddDays(1);
+        var yesterday = dateTime.AddDays(-1);
+        Console.WriteLine(tomorrow);
+        Console.WriteLine(yesterday);
+
+        Console.WriteLine(now.ToLongDateString());
+        Console.WriteLine(now.ToShortDateString());
+        Console.WriteLine(now.ToLongTimeString());
+        Console.WriteLine(now.ToShortTimeString());
+        Console.WriteLine(now.ToString());
+        Console.WriteLine(now.ToString("yyyy-MM-dd"));
+        Console.WriteLine(now.ToString("HH-mm"));
+    }
+}
